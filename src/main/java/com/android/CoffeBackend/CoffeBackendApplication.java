61962eda944ef7aea7fc61b1db593d41917e654b
LuiDeV <contactoluixzhc@gmail.com>
2026-09-11 15:53:53 -0500

    Backend CavoshCafe

---
+++ src/main/java/com/android/CoffeBackend/CoffeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.android.CoffeBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/android/CoffeBackend/CoffeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.android.CoffeBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
